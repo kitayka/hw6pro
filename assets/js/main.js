883e4b1cb1c5ae238770da0e661b2abe8551bdc6
Oleksandr Kitaika <kitayka96@gmail.com>
2020-04-30 02:01:51 +0300

    hw6pro

---
+++ assets/js/main.js
@@ -0,0 +1,64 @@
+"use strict";
+function Circle(radius) {
+    this.radius = radius;   
+    // this.radius = prompt("Введите радиус окружности: ");       
+
+    this.calcCircleArea = function() {
+        return (Math.PI * Math.pow(this.radius, 2));
+    }
+}
+
+let circle = new Circle(5);
+// let circle = new Circle();
+console.log("Площадь окружности: " + circle.calcCircleArea());
+
+
+
+
+
+function CircleLength (radius) {
+    this.radius = radius;
+
+    this.calcCircleLength = function() {
+        return (2 * Math.PI * radius);
+    }
+}
+
+let circleLength = new CircleLength(5);
+console.log("Длина окружности: " + circleLength.calcCircleLength());
+
+
+
+
+function ArithmeticMean (a, b) {
+    this.a = a;
+    this.b = b;
+
+    this.calcArithmeticMean = function() {
+        return ((a + b)/ 2);
+    }
+}
+
+let arithmeticMean = new ArithmeticMean(10, 20);
+console.log("Среднее арифметическое двух чисел: " + arithmeticMean.calcArithmeticMean());
+
+
+
+
+
+
+
+function ArrMean(arr) {
+    this.arr = arr;
+
+    this.calcArrMean = function() {
+        let sum = 0;
+        for (let i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        return (sum / arr.length);
+    }
+}
+
+let arrMean = new ArrMean([5, 5, 10, 10, 15]);
+console.log("Среднее арифметическое массива: " + arrMean.calcArrMean());
